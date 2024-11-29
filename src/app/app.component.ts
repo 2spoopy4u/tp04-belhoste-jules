4d9c76497c04759abf4badc2eaa2ac69655f1fc0
jbelhoste <jules.belhoste@etu.unistra.fr>
2024-11-29 22:36:14 +0100

    Modification des  imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {FormComponent} from './credit-card-manager/form/form.component'
-import { ListComponent } from './credit-card-manager/list/list.component';
 import { CreditCardManagerModule } from './credit-card-manager/credit-card-manager.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CreditCardManagerModule],
+  imports: [CreditCardManagerModule],
   styleUrl: './app.component.css',
   template:"<div><app-form/><br/><app-list/></div>"
 })
